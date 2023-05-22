6004bb0369cc9db8a22260499a806cc6e11d3797
Agner <agnerf@gmail.com>
2023-05-21 23:46:05 -0300

    implementation

---
+++ data/data.js
@@ -0,0 +1,15 @@
+function dados(){
+    
+    const progs = [{
+        nome: "Prog 1",
+        icone: "./icones/prog1.svg"
+    },{
+        nome: "Prog 2",
+        icone: "./icones/prog2.svg"
+    },{
+        nome: "Prog 3",
+        icone: "./icones/prog3.svg"
+    }]
+
+    return progs
+}
---
+++ scripts/main.js
@@ -0,0 +1,63 @@
+dados = dados()
+
+//Simula um tempo de carregamento
+main()
+
+//Cria os principais elementos da página
+function main(progs){
+    const body = document.querySelector("body")
+
+    const container = document.createElement("div")
+    container.classList.add("ui-container")
+    body.appendChild(container)
+
+    const barra = document.createElement("div")
+    barra.classList.add("barra-superior")
+    container.appendChild(barra)
+    criarBarra(barra)
+
+    const desktop = document.createElement("div")
+    desktop.classList.add("desktop")
+    container.appendChild(desktop)
+
+    const dock = document.createElement("div")
+    dock.classList.add("dock")
+    container.appendChild(dock)
+    criarDock(dock)
+}
+
+function criarBarra(barra){
+    const data = document.createElement("div")
+    atualizarHora(data)
+    data.classList.add("data")
+    barra.appendChild(data)
+}
+
+function atualizarHora(data){
+    const date = new Date()
+    data.innerText = date.getDate() + "/" + date.getMonth() + " - " + date.getHours() + ":" + date.getMinutes()
+    setInterval(() => {
+        data.innerText = date.getDate() + "/" + date.getMonth() + " - " + date.getHours() + ":" + date.getMinutes()
+    }, 3000);
+}
+
+function criarDock(dock){
+    
+    dados.forEach(dado => {    
+        const item = document.createElement("div")
+        item.classList.add("item-prog")
+
+        const nomeProg = document.createElement("div")
+        nomeProg.classList.add("nome-prog")
+        nomeProg.innerHTML = `<p>${dado.nome}</p>`
+        item.appendChild(nomeProg)
+
+        const iconeProg = document.createElement("img")
+        iconeProg.classList.add("icone-prog")
+        iconeProg.src = `url(${dado.icone})`
+        item.appendChild(iconeProg)
+
+        dock.appendChild(item)
+    });
+
+}
